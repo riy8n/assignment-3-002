fb0057fa6302f3c04144572055f6a0cec5ab4bdd
riy8n <mahafujhossainriyan02@gmail.com>
2025-01-31 01:46:07 +0600

    added the header and the hero section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors:{
+            customBeige: 'rgb(239, 235, 227)',
+        }
+      },
+    },
+    plugins: [],
+  }
